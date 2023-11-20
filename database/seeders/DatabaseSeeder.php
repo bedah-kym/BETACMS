f3d12e0cc5c8e972d3b96b5d00d9bf55b40d3409
Chelule Maina <chelumaina@gmail.com>
2023-11-20 15:58:49 +0300

    Dump

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Schema::disableForeignKeyConstraints();
-        $this->call(AuthTableSeeder::class);
+        // $this->call(AuthTableSeeder::class);
         $this->call(CaseTypesTableSeeder::class);
         Schema::enableForeignKeyConstraints();
     }
